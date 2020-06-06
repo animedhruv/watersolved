9fb5a4182181ee500f3894c13b5cc1065e22640e
HJCJackLuo <30420522+HJCJackLuo@users.noreply.github.com>
2020-06-06 14:24:07 -0700

    Updates

---
+++ precache-manifest.69816a4d0eda3beca0c9216ee8a091ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39ffa499aa462d79be6fb7c7388d5bae",
+    "url": "/watersolved/index.html"
+  },
+  {
+    "revision": "c1e2fbbecaccaefe1595",
+    "url": "/watersolved/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "73775434c79893de4437",
+    "url": "/watersolved/static/css/main.6d14eae6.chunk.css"
+  },
+  {
+    "revision": "c1e2fbbecaccaefe1595",
+    "url": "/watersolved/static/js/2.276cd56e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/watersolved/static/js/2.276cd56e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73775434c79893de4437",
+    "url": "/watersolved/static/js/main.98400f3e.chunk.js"
+  },
+  {
+    "revision": "af1a068408284986ba0d",
+    "url": "/watersolved/static/js/runtime-main.41b73692.js"
+  }
+]);
